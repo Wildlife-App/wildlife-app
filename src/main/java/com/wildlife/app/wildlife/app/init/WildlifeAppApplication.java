01de24c4910d9c384a180266a84363a4b83990f3
Arnab Mondal <arnabp2601@gmail.com>
2020-04-17 18:01:09 +0530

    Add files via upload

---
+++ src/main/java/com/wildlife/app/wildlife/app/init/WildlifeAppApplication.java
@@ -0,0 +1,13 @@
+package com.wildlife.app.wildlife.app.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildlifeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildlifeAppApplication.class, args);
+	}
+
+}
